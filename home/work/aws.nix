31dbd53d07b41e1be4b9d61b7b4844bc937ea37c
Alexey Raga <alexey.raga@gmail.com>
2023-03-13 11:54:46 +1100

    Add dev profile

--- home/work/aws.nix
+++ home/work/aws.nix
@@ -38,6 +38,14 @@ in
         region = "ap-southeast-2";
       };
 
+      dev = {
+        sso_start_url = "https://educationperfect.awsapps.com/start";
+        sso_account_id = "327162508743";
+        sso_role_name = "admin";
+        sso_region = "ap-southeast-2";
+        region = "ap-southeast-2";
+      };
+
       test-unsafe = {
         sso_start_url = "https://educationperfect.awsapps.com/start";
         sso_account_id = "327162508743";
