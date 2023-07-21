66770bdda8d595520fb36a181b86b1db30c9674c
Alexey Raga <alexey.raga@gmail.com>
2023-07-21 23:15:27 +1000

    Configure VSCode with Nix, fix Homebrew

--- darwin/apps.nix
+++ darwin/apps.nix
@@ -1,17 +1,55 @@
 { config, lib, pkgs, ... }:
-{
 
+{
   darwin.apps = {
     raycast.enable = true;
-    iterm2.enable = true; # best terminal ever
-  };
+    iterm2.enable = true;
 
-  home-manager.users."${config.user.name}".home = {
-    packages = with pkgs; [ m-cli ];
+    vscode = {
+      enable = true;
+      extensions = [
+        "AdamCaviness.theme-monokai-dark-soda"
+        "GraphQL.vscode-graphql"
+        "GraphQL.vscode-graphql-syntax"
+        "Intility.vscode-backstage"
+        "Ionide.Ionide-fsharp"
+        "PKief.material-icon-theme"
+        "akamud.vscode-theme-onedark"
+        "amazonwebservices.aws-toolkit-vscode"
+        "arrterian.nix-env-selector"
+        "bbenoist.Nix"
+        "bierner.markdown-mermaid"
+        "coolbear.systemd-unit-file"
+        "donjayamanne.githistory"
+        "eamodio.gitlens"
+        "giltho.comby-vscode"
+        "hashicorp.terraform"
+        "haskell.haskell"
+        "jnoortheen.nix-ide"
+        "joaompinto.vscode-graphviz"
+        "justusadam.language-haskell"
+        "kmoritak.vscode-mermaid-snippets"
+        "ms-azuretools.vscode-docker"
+        "ms-dotnettools.csdevkit"
+        "ms-dotnettools.csharp"
+        "ms-dotnettools.vscode-dotnet-runtime"
+        "ms-dotnettools.vscodeintellicode-csharp"
+        "ms-vscode-remote.remote-containers"
+        "ms-vscode.makefile-tools"
+        "ms-vsliveshare.vsliveshare-pack"
+        "redhat.vscode-yaml"
+        "streetsidesoftware.avro"
+        "vscode-icons-team.vscode-icons"
+        "zxh404.vscode-proto3"
+      ];
+    };
   };
 
   homebrew = {
     enable = true;
+
+    global.brewfile = true;
+    onActivation.cleanup = "zap";
 
     taps = [
       "homebrew/cask"
@@ -36,7 +74,6 @@
       "slack"
       "sublime-text"
       "telegram"
-      "visual-studio-code"
       "vlc"
       "warp" # new terminal
       "zoom"
---
+++ darwin/modules/apps/vscode.nix
@@ -0,0 +1,62 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  enabled = cfg.enable && pkgs.hostPlatform.isDarwin;
+  cfg = config.darwin.apps.vscode;
+
+  codeBin = ''/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'';
+
+in
+{
+  options.darwin.apps.vscode = {
+    enable = mkEnableOption "Enable VSCode";
+
+    extensions =  mkOption {
+       type = types.listOf types.str;
+       default = [];
+       description = "List of required extensions";
+    };
+  };
+
+  config = mkIf enabled {
+    # Install VSCode from Homebrew
+    homebrew = {
+      taps = [ "homebrew/cask" ];
+      casks = [ "visual-studio-code" ];
+    };
+
+    system.activationScripts.postUserActivation.text =
+      let currentBundleContent = builtins.concatStringsSep "\n" (lib.lists.unique (lib.lists.sort (a: b: a < b) cfg.extensions));
+      in ''
+      normalColor="$(tput sgr0)"
+      noteColor="$(tput bold)$(tput setaf 6)"
+
+      bundle_file="$HOME/.vscode/extensions/extensions.nix.bundle"
+
+      # Sort the arrays and remove duplicates
+      installed_exts=$(${codeBin} --list-extensions | sort -u)
+      wanted_exts="${currentBundleContent}"
+
+      [ -f "$bundle_file" ] && bundled_exts=$(cat $bundle_file | sort -u) || bundled_exts=""
+
+      to_install=( $(comm -23 <(echo "$wanted_exts") <(echo "$installed_exts")) )
+
+      # Find elements that are in both stored_exts and installed_exts but not in wanted_exts
+      to_remove=($(comm -23 <(echo "$bundled_exts") <(echo "$wanted_exts") | comm -12 <(echo "$installed_exts") -))
+
+      echo "''${noteColor}Setting VSCode extensions''${normalColor}"
+
+      if [ ''${#to_install[@]} -gt 0 ]; then
+        $DRY_RUN_CMD ${codeBin} ''${to_install[*]/#/--install-extension }
+      fi
+
+      if [ ''${#to_remove[@]} -gt 0 ]; then
+        $DRY_RUN_CMD ${codeBin} ''${to_remove[*]/#/--uninstall-extension }
+      fi
+
+      echo "${currentBundleContent}" > $bundle_file
+    '';
+  };
+}
