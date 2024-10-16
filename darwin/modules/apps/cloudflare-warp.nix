0ef374753c34bd4f99a82860b2406cdb85cef6ca
Alexey Raga <alexey.raga@gmail.com>
2024-10-16 14:53:07 +1100

    Fix warp-switch to work with modern warp-cli

--- darwin/modules/apps/cloudflare-warp.nix
+++ darwin/modules/apps/cloudflare-warp.nix
@@ -17,16 +17,7 @@ let
   warpVnetShow = pkgs.writeShellScriptBin "warp-show" ''
 warp-cli status | grep -q "Disconnected" && exit 0 || true
 
-NETWORKS=$(warp-cli vnet)
-
-CURRENT_NETWORK=$(echo "$NETWORKS" | grep 'Currently selected' | awk '{print $NF}')
-
-if [ -z "$CURRENT_NETWORK" ]; then
-    echo "You are currently not connected to any VPN"
-    exit 1
-fi
-
-CURRENT_NETWORK_NAME=$(echo "$NETWORKS" | grep -A 1 "ID: $CURRENT_NETWORK" | grep 'Name:' | awk '{print $NF}')
+CURRENT_NETWORK_NAME=$(warp-cli --json vnet | jq -r '. as $data | $data.virtual_networks[] | select(.id == $data.active_vnet_id) | .name')
 
 echo $CURRENT_NETWORK_NAME
   '';
@@ -51,7 +42,7 @@ EOF
         warp-cli disconnect > /dev/null
         ;;
       *)
-        warp-cli vnet | grep -B1 "Name: $1" | head -n 1 | cut -d' ' -f2 | xargs warp-cli vnet
+        warp-cli --json vnet | jq -r --arg PREFIX "$1" '.virtual_networks[] | select(.name == $PREFIX or .name == ($PREFIX + "-vnet")) | .id' | head -n 1 | xargs warp-cli vnet
         warp-cli connect > /dev/null
         ;;
     esac
@@ -61,7 +52,7 @@ EOF
     #compdef warp-switch
     _warp_cli_get_virtual_networks() {
       local -a subnet_names
-      subnet_names=($(warp-cli vnet | grep 'Name: ' | cut -d' ' -f2 | grep -v "default"))
+      subnet_names=($(warp-cli --json vnet | jq -r '.virtual_networks[].name' | grep -v "default"))
       compadd $subnet_names
     }
 
