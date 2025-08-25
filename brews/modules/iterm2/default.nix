1f3df227287acb30dd0c7f56be2fb0fc6ca0c1e1
Alexey Raga <alexey.raga@gmail.com>
2025-08-25 19:22:14 +1000

    Install themes

--- brews/modules/iterm2/default.nix
+++ brews/modules/iterm2/default.nix
@@ -4,6 +4,25 @@ with lib;
 
 let
   cfg = config.brews.iterm2;
+  
+  # Create a derivation containing all theme files
+  themesPackage = pkgs.runCommand "iterm2-themes" {} ''
+    mkdir -p $out
+    cp ${./themes}/*.itermcolors $out/
+  '';
+  
+  # Function to get all available themes from the package
+  themes = 
+    let
+      dirContents = builtins.readDir "${themesPackage}";
+      themeFiles = lib.filterAttrs (name: type: 
+        type == "regular" && lib.hasSuffix ".itermcolors" name
+      ) dirContents;
+    in
+    lib.mapAttrsToList (filename: type: {
+      name = lib.removeSuffix ".itermcolors" filename;
+      file = "${themesPackage}/${filename}";
+    }) themeFiles;
 
   shell_integration = pkgs.fetchFromGitHub {
     name = "iterm2-shell-integration";
@@ -12,6 +31,7 @@ let
     rev = "4999d188aba9e470fa921367288ab6d5074b5324";
     sha256 = "sha256-HXmZty8emMoUtyuJpLLY+IfHBIJjG9wUJNGv0a3hJBc=";
   };
+
   utilities = builtins.attrNames (builtins.readDir "${shell_integration}/utilities");
   aliases = lib.concatMapStringsSep ";" (x: "alias ${x}='${shell_integration}/utilities/${x}'") utilities;
 in
@@ -91,368 +111,30 @@ in
       ${aliases}
     '';
 
-		home.activation.configureIterm = 
-			let 
-				plist = "~/Library/Preferences/com.googlecode.iterm2.plist"; 
-			in lib.hm.dag.entryAfter [ "linkGeneration" ] ''
-			/usr/libexec/PlistBuddy -c "Add :'Custom Color Presets':'Nightfox' dict" ${plist} >/dev/null 2>&1 || true
-			/usr/libexec/PlistBuddy -c "Merge '${config.xdg.configFile."iterm2/nightfox.itermcolors".source}' :'Custom Color Presets':'Nightfox'" ${plist}
+    # Dynamic theme configuration
+    home.activation.configureIterm = 
+      let 
+        plist = "~/Library/Preferences/com.googlecode.iterm2.plist"; 
+        
+        # Generate commands to install all themes
+        themeInstallCommands = lib.concatMapStringsSep "\n" (theme: 
+          ''
+            /usr/libexec/PlistBuddy -c "Add :'Custom Color Presets':'${theme.name}' dict" ${plist} >/dev/null 2>&1 || true
+            /usr/libexec/PlistBuddy -c "Merge '${theme.file}' :'Custom Color Presets':'${theme.name}'" ${plist}
+          ''
+        ) themes;
 
-			/usr/libexec/PlistBuddy \
-				-c "Set :'New Bookmarks':0:'Normal Font' '${cfg.font}'" \
-				-c "Set :'New Bookmarks':0:'Columns' ${toString cfg.columns}" \
-				-c "Set :'New Bookmarks':0:'Rows' ${toString cfg.rows}" \
-				-c "Set :'New Bookmarks':0:'Silence Bell' 1" \
-				-c "Set :'New Bookmarks':0:'Custom Directory' Recycle" \
-				-c "Set :'New Bookmarks':0:'Guid' nightfox" \
-				${plist}
-			'';
+      in lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+        # Install all available themes
+        ${themeInstallCommands}
 
-    xdg.configFile."iterm2/nightfox.itermcolors".text = ''
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-    	<key>Ansi 0 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.26666668057441711</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.23137255012989044</real>
-    		<key>Red Component</key>
-    		<real>0.22352941334247589</real>
-    	</dict>
-    	<key>Ansi 1 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.42745098471641541</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.30980393290519714</real>
-    		<key>Red Component</key>
-    		<real>0.78823530673980713</real>
-    	</dict>
-    	<key>Ansi 10 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.54509806632995605</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.80392158031463623</real>
-    		<key>Red Component</key>
-    		<real>0.34509804844856262</real>
-    	</dict>
-    	<key>Ansi 11 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.49411764740943909</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.89019608497619629</real>
-    		<key>Red Component</key>
-    		<real>1</real>
-    	</dict>
-    	<key>Ansi 12 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.89411765336990356</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.80784314870834351</real>
-    		<key>Red Component</key>
-    		<real>0.51764708757400513</real>
-    	</dict>
-    	<key>Ansi 13 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.89019608497619629</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.63137257099151611</real>
-    		<key>Red Component</key>
-    		<real>0.72156864404678345</real>
-    	</dict>
-    	<key>Ansi 14 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>1</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.94117647409439087</real>
-    		<key>Red Component</key>
-    		<real>0.3490196168422699</real>
-    	</dict>
-    	<key>Ansi 15 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.94901961088180542</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.94901961088180542</real>
-    		<key>Red Component</key>
-    		<real>0.94901961088180542</real>
-    	</dict>
-    	<key>Ansi 2 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.60392159223556519</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.69803923368453979</real>
-    		<key>Red Component</key>
-    		<real>0.5058823823928833</real>
-    	</dict>
-    	<key>Ansi 3 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.45490196347236633</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.75294119119644165</real>
-    		<key>Red Component</key>
-    		<real>0.85882353782653809</real>
-    	</dict>
-    	<key>Ansi 4 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.83921569585800171</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.61176472902297974</real>
-    		<key>Red Component</key>
-    		<real>0.44313725829124451</real>
-    	</dict>
-    	<key>Ansi 5 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.83921569585800171</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.47450980544090271</real>
-    		<key>Red Component</key>
-    		<real>0.61568629741668701</real>
-    	</dict>
-    	<key>Ansi 6 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.81176471710205078</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.80392158031463623</real>
-    		<key>Red Component</key>
-    		<real>0.38823530077934265</real>
-    	</dict>
-    	<key>Ansi 7 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.87843137979507446</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.87450981140136719</real>
-    		<key>Red Component</key>
-    		<real>0.87450981140136719</real>
-    	</dict>
-    	<key>Ansi 8 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.44705882668495178</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.31372550129890442</real>
-    		<key>Red Component</key>
-    		<real>0.27843138575553894</real>
-    	</dict>
-    	<key>Ansi 9 Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.41960784792900085</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.3803921639919281</real>
-    		<key>Red Component</key>
-    		<real>0.83921569585800171</real>
-    	</dict>
-    	<key>Background Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.18823529779911041</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.13725490868091583</real>
-    		<key>Red Component</key>
-    		<real>0.098039217293262482</real>
-    	</dict>
-    	<key>Badge Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>0.5</real>
-    		<key>Blue Component</key>
-    		<real>0.0</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.14910027384757996</real>
-    		<key>Red Component</key>
-    		<real>1</real>
-    	</dict>
-    	<key>Bold Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.99999994039535522</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.99999994039535522</real>
-    		<key>Red Component</key>
-    		<real>1</real>
-    	</dict>
-    	<key>Cursor Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.78104287385940552</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.78104287385940552</real>
-    		<key>Red Component</key>
-    		<real>0.78104299306869507</real>
-    	</dict>
-    	<key>Cursor Guide Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>0.25</real>
-    		<key>Blue Component</key>
-    		<real>1</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.92681378126144409</real>
-    		<key>Red Component</key>
-    		<real>0.70214027166366577</real>
-    	</dict>
-    	<key>Cursor Text Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.99999994039535522</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.99999994039535522</real>
-    		<key>Red Component</key>
-    		<real>1</real>
-    	</dict>
-    	<key>Foreground Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.81176471710205078</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.80784314870834351</real>
-    		<key>Red Component</key>
-    		<real>0.80392158031463623</real>
-    	</dict>
-    	<key>Link Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.73422712087631226</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.35915297269821167</real>
-    		<key>Red Component</key>
-    		<real>0.0</real>
-    	</dict>
-    	<key>Selected Text Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>0.0</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.0</real>
-    		<key>Red Component</key>
-    		<real>0.0</real>
-    	</dict>
-    	<key>Selection Color</key>
-    	<dict>
-    		<key>Alpha Component</key>
-    		<real>1</real>
-    		<key>Blue Component</key>
-    		<real>1</real>
-    		<key>Color Space</key>
-    		<string>sRGB</string>
-    		<key>Green Component</key>
-    		<real>0.86968445777893066</real>
-    		<key>Red Component</key>
-    		<real>0.75813823938369751</real>
-    	</dict>
-    </dict>
-    </plist>
-  '';
+        /usr/libexec/PlistBuddy \
+          -c "Set :'New Bookmarks':0:'Normal Font' '${cfg.font}'" \
+          -c "Set :'New Bookmarks':0:'Columns' ${toString cfg.columns}" \
+          -c "Set :'New Bookmarks':0:'Rows' ${toString cfg.rows}" \
+          -c "Set :'New Bookmarks':0:'Silence Bell' 1" \
+          -c "Set :'New Bookmarks':0:'Custom Directory' Recycle" \
+          ${plist}
+      '';
   };
 }
