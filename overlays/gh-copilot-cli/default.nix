0d8821d064f876c7439224e8823f59d570502d36
Alexey Raga <alexey.raga@gmail.com>
2025-11-15 18:18:25 +1100

    Upgrade CoPilot

--- overlays/gh-copilot-cli/default.nix
+++ overlays/gh-copilot-cli/default.nix
@@ -1,16 +1,16 @@
 self: super: {
   gh-copilot-cli = super.buildNpmPackage rec {
     pname = "gh-copilot-cli";
-    version = "0.0.353";
+    version = "0.0.358";
 
     nodejs = super.nodejs_22;
 
     src = super.fetchzip {
       url = "https://registry.npmjs.org/@github/copilot/-/copilot-${version}.tgz";
-      hash = "sha256-OWlEz75vVEvbtDNobLJ/a1iUuepYewCTWoqTbDG+4wg=";
+      hash = "sha256-+OjC5m5KnZrp0qquMT/Nf4xFoXS/Dz7XBG8djwktYMk=";
     };
 
-    npmDepsHash = "sha256-O9xJvRjzq1mxs9GfxcABvEayCxPMtbDP5hP80Op/RdI=";
+    npmDepsHash = "sha256-piJp0VHIoNPQE3gqIZVrIw2HHJffkNgxf2U3PNv6Xiw=";
 
     postPatch = ''
       mkdir -p node_modules
